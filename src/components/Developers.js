324c38a77779763b0172bed1710b7b2474ad4591
vaibhav <118048300+Vaibhavoberoi07@users.noreply.github.com>
2024-02-14 20:01:27 +0530

    first commit

--- src/components/Developers.js
+++ src/components/Developers.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Developers = () => {
   return (
     <div className='developers' id='Developer'>
-      <h1>Developers Developer Hub </h1>
+      <h1 className='margin'>Developers Developer Hub </h1>
       <h1>Build on the Future of</h1>
       <span className='color'>Blockchain.</span>
     </div>
